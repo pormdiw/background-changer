f91977dac00a39fa1c13e70435f9ece42b4800b7
pormdiw <84891610+pormdiw@users.noreply.github.com>
2021-07-01 20:27:43 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,48 @@
+var btn_red;
+var btn_green;
+
+var r = 0;
+var g = 0;
+var b = 0;
+
+function setup() {
+  createCanvas(400, 400);  
+
+  btn_red = createButton("Red");
+  btn_red = position(100,50);
+  btn_red = mousepressed(red_bg);
+
+  btn_green = createButton("Green");
+  btn_green = position(250,50);
+  btn_green = mousepressed(green_bg);
+
+}
+function draw() {
+  background(r,g,b);
+
+function red_bg(){
+
+  r = 255;
+  g = 0;
+  b = 0;
+}
+
+function green_bg(){
+
+  r = 0;
+  g = 255;
+  b = 0
+
+if(btn_green){
+
+  background ="green";
+
+}
+
+if(btn_red){
+
+  background ="red";
+
+}
+}
+}
